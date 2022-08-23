3a3ea9e2f85148855dcf3c77fdc263d0c5910c7c
Azeddine El makkaoui <ael-makk@e3r1p13.1337.ma>
2022-08-23 16:47:04 +0100

    added more ressources to level01

--- level00/source.c
+++ level00/source.c
@@ -3,8 +3,8 @@ int main(int argc, char **argv)
     int value;
     value = atoi(argv[1]);
     if (value != 423)
-    {}
+    {} // {"flag"}
     else
-        fwrite();
+        fwrite(); // {"No\n"}
     return (0);
 }
